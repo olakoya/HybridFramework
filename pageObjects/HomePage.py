cf5f52ff88efadc95efb4e74a900214a4b56e41d
olakoya <ola.koya1@gmail.com>
2025-04-30 20:31:07 +0100

    Completed HomePage & AccountRegistrationPage files

---
+++ pageObjects/HomePage.py
@@ -0,0 +1,22 @@
+from selenium.webdriver.common.by import By
+
+# Creating Class and inside it contains locators
+class HomePage:
+    # Locators
+    lnk_myaccount_xpath = "//span[text()='My Account']"
+    lnk_register_linktext = "Register"
+    lnk_login_linktext = "Login"
+
+# Creating a Constructor
+    def __init__(self,driver): # constructor has a 'driver' variable
+        self.driver = driver
+
+# Creating a Click Action Methods
+    def clickMyAccount(self):
+        self.driver.find_element(By.XPATH,self.lnk_myaccount_xpath).click() # locator declared earlier in class
+
+    def clickRegister(self):
+        self.driver.find_element(By.XPATH,self.lnk_register_linktext).click()
+
+    def clickLogin(self):
+        self.driver.find_element(By.XPATH,self.lnk_login_linktext).clcik()
