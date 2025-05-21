c57e1ade1d2745a1bdaf3336d89f972a004579c4
olakoya <ola.koya1@gmail.com>
2025-05-21 20:17:18 +0100

    Created XLUtils file and .xlsx file

---
+++ utilities/XLUtils.py
@@ -0,0 +1,16 @@
+import openpyxl
+from openpyxl.styles import PatternFill
+
+def getRowCount(file,sheetName):
+    workbook = openpyxl.load_workbook(file)
+    sheet = workbook[sheetName]
+    return(sheet.max_row)
+def getColumnCount(file,sheetName):
+    workbook = openpyxl.load_workbook(file)
+    sheet = workbook[sheetName]
+    return (sheet.max_row)
+def readData(file,sheetName,rownum,columnno,data):
+    workbook = openpyxl.load_workbook(file)
+    sheet = workbook[sheetName]
+    sheet.cell(rownum, columnno).value = data
+    workbook.save(file)
