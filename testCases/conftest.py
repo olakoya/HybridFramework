460abb96029d6521e63baefa82881c2e2cd6b09b
olakoya <ola.koya1@gmail.com>
2025-06-11 20:54:00 +0100

    Updates

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -1,132 +1,149 @@
-import os.path
-from email.policy import default
-
-from pytest_metadata.plugin import metadata_key
-import pytest
-from numpy.f2py.rules import options
-from selenium import webdriver
+import os
 from datetime import datetime
+import pytest
+from selenium import webdriver
 from utilities.readProperties import ReadConfig
+from pytest_metadata.plugin import metadata_key
 
-@pytest.fixture()
-def setup(browser_platform):
-    baseenv = ReadConfig.getEnvironment()
-    browser_platform = browser_platform
-    if baseenv == "remote":
-        options = {
-            "chrome": webdriver.ChromeOptions,
-            "edge": webdriver.EdgeOptions,
-            "firefox": webdriver.FirefoxOptions
-        }
-        if browser not in options:
-            raise ValueError(f"Unsupported browser:{browser}")
-        platform_mapping = {"mac": "MAC", "windows": "Win10", "linux": "LINUX"}
-        platform_name = platform_mapping.get(platform)
-        if not platform_name:
-            raise ValueError(f"Unsupported platform: {platform}")
-        opt = options[browser]()
-        opt.add_experimental_option("detach", True) if browser in ["chrome", "edge"] else None
-        opt.platform_name = platform_name
-        driver = webdriver.Remote(command_executor="http://localhost:4444/wd/hub", options=opt)
-    elif baseenv == "local":
-        if browser == 'edge':
-            options = webdriver.EdgeOptions()
-            options.add_experimental_option("detach", True)
-            driver = webdriver.Edge(options=options)
-            print("Launching Edge browser.........")
-        if browser == 'firefox':
-            options = webdriver.FirefoxOptions()
-            driver = webdriver.Firefox(options=options)
-            print("Launching Edge browser.........")
-        else:
-            options = webdriver.ChromeOptions()
-            options.add_experimental_option("detach", True)
-            driver = webdriver.Chrome(options=options)
-            print("Launching Chrome browser.........")
-        yield driver
-        driver.quit()
+# Add custom CLI options
+def pytest_addoption(parser):
+    parser.addoption("--browser", default="chrome", choices=["chrome", "edge", "firefox"], help="Browser to test")
+    parser.addoption("--os", default="mac", choices=["windows", "mac", "linux"], help="Operating System to test")
 
-# Hook to add commandline options for browser and os
-def pytest_addoption(parser): # This will get the value from CLI/Hooks
-    parser.addoption("--browser", default="chrome", choices=["chrome", "edge", "firefox"], help = "Browser to test")
-    parser.addoption("--os", default="mac", choices=["windows", "mac", "linux"], help = "Operating System to test")
-
-# Get value from commandline
+# Parse CLI options into a tuple
 @pytest.fixture()
 def browser_platform(request):
     browser = request.config.getoption("--browser")
     platform = request.config.getoption("--os")
-    return browser,platform
+    return browser, platform
 
-#Hook for Adding Environment info to HTML Report
-def pytest_configure(config):
-    config.stash[metadata_key]['Project Name'] = 'Tutorial Ninja'
-    config.stash[metadata_key]['Tester Name'] = 'Ola'
-
-#Hook for Delete/Modify Environment info to HTML Report
-@pytest.hookimpl(optionalhook=True)
-def pytest_metadata(metadata):
-    metadata.pop("Python", None)
-    metadata.pop("Plugins", None)
-
-#Specifying Report Folder Locations and Save Report with Timestamp
-@pytest.hookimpl(tryfirst=True)
-def pytest_configure(config):
-    config.option.htmlpath = (os.path.dirname(os.getcwd()) + "HybridFramework//reports//"+datetime.now().strftime("%d-%m-%Y %H-%M-%S") + ".html")
-
-
-
-
-
-# First Conftest commanline
+# Main fixture for driver setup
 @pytest.fixture()
-def setup(browser): # Passing fixture function
+def setup(browser_platform):
+    browser, platform = browser_platform
+    baseenv = ReadConfig.getEnvironment()  # 'local' or 'remote'
 
-        if browser == 'edge':
+    if baseenv == "remote":
+        options_map = {
+            "chrome": webdriver.ChromeOptions,
+            "edge": webdriver.EdgeOptions,
+            "firefox": webdriver.FirefoxOptions
+        }
+
+        if browser not in options_map:
+            raise ValueError(f"Unsupported browser: {browser}")
+
+        opt = options_map[browser]()
+        if browser in ["chrome", "edge"]:
+            opt.add_experimental_option("detach", True)
+
+        platform_mapping = {"mac": "MAC", "windows": "WIN10", "linux": "LINUX"}
+        platform_name = platform_mapping.get(platform.lower())
+        if not platform_name:
+            raise ValueError(f"Unsupported platform: {platform}")
+
+        opt.set_capability("platformName", platform_name)
+
+        driver = webdriver.Remote(
+            command_executor="http://localhost:4444/wd/hub",
+            options=opt
+        )
+        print(f"Launching {browser.capitalize()} on Selenium Grid ({platform})...")
+
+    else:  # Local execution
+        if browser == "edge":
             options = webdriver.EdgeOptions()
             options.add_experimental_option("detach", True)
             driver = webdriver.Edge(options=options)
-            driver.maximize_window()
-            print("Launching Edge Browser.........")
-
-        elif browser == 'firefox':
+        elif browser == "firefox":
             options = webdriver.FirefoxOptions()
             driver = webdriver.Firefox(options=options)
-            driver.maximize_window()
-            print("Launching Firefox Browser.........")
-
         else:
             options = webdriver.ChromeOptions()
             options.add_experimental_option("detach", True)
             options.add_argument("--start-maximized")
             driver = webdriver.Chrome(options=options)
-            print("Launching Chrome Browser.........")
 
-        yield driver
-        driver.quit()
+        print(f"Launching {browser.capitalize()} browser locally...")
 
-def pytest_addoption(parser): # This will get the value from CLI/Hooks
-    parser.addoption('--browser')
+    yield driver
+    driver.quit()
 
-@pytest.fixture()
-def browser(request): # This will return the Browser value to setup method
-    return request.config.getoption('--browser')
+# Hooks for HTML report metadata and to add environment info to HTML report
+@pytest.hookimpl(tryfirst=True)
+def pytest_configure(config):
+    report_dir = os.path.dirname(os.getcwd()) + "//HybridFramework//reports//"
+    timestamp = datetime.now().strftime("%d-%m-%Y %H-%M-%S")
+    config.option.htmlpath = os.path.join(report_dir, f"{timestamp}.html")
 
-# It is hook for adding Environment info to HTML report
-def pytest_configure(config): # For adding information hook isn't required
-    config.stash[metadata_key]['Project Name'] = 'Tutorial Ninja'
-    config.stash[metadata_key]['Module Name'] = 'CustRegistration'
-    config.stash[metadata_key]['Tester Name'] = 'Ola'
+    if hasattr(config, "_metadata"):
+        config._metadata['Project Name'] = 'Tutorial Ninja'
+        config._metadata['Module Name'] = 'CustRegistration'
+        config._metadata['Tester Name'] = 'Ola'
 
-# It is hook for delete/modify Environment info to HTML report
+    # config.stash[metadata_key]['Project Name'] = 'Tutorial Ninja'
+    # config.stash[metadata_key]['Module Name'] = 'CustRegistration'
+    # config.stash[metadata_key]['Tester Name'] = 'Ola'
+
+
+# Hook to clean up unnecessary metadata
 @pytest.hookimpl(optionalhook=True)
-# @pytest.mark.optionalhook # Deprecated
-def pytest_metadata(metadata): # For modifying hook is required above
-    metadata.pop("Python", None)  # pop means remove or delete
+def pytest_metadata(metadata):
+    metadata.pop("Python", None)
     metadata.pop("Plugins", None)
 
-# Specifying report folder location and save report with timestamp
-@pytest.hookimpl(tryfirst=True) # Using hook for one configuration which is used from the scratch to generate this report dynamically and it's automatically executed
-def pytest_configure(config):
-    config.option.htmlpath = (os.path.dirname(os.getcwd()) + "//HybridFramework//reports//"+datetime.now().strftime("%d-%m-%Y %H-%M-%S")+".html")
-    # Your existing code
+
+
+
+# First Conftest commanline
+# @pytest.fixture()
+# def setup(browser): # Passing fixture function
+#
+#         if browser == 'edge':
+#             options = webdriver.EdgeOptions()
+#             options.add_experimental_option("detach", True)
+#             driver = webdriver.Edge(options=options)
+#             driver.maximize_window()
+#             print("Launching Edge Browser.........")
+#
+#         elif browser == 'firefox':
+#             options = webdriver.FirefoxOptions()
+#             driver = webdriver.Firefox(options=options)
+#             driver.maximize_window()
+#             print("Launching Firefox Browser.........")
+#
+#         else:
+#             options = webdriver.ChromeOptions()
+#             options.add_experimental_option("detach", True)
+#             options.add_argument("--start-maximized")
+#             driver = webdriver.Chrome(options=options)
+#             print("Launching Chrome Browser.........")
+#
+#         yield driver
+#         driver.quit()
+#
+# def pytest_addoption(parser): # This will get the value from CLI/Hooks
+#     parser.addoption('--browser')
+#
+# @pytest.fixture()
+# def browser(request): # This will return the Browser value to setup method
+#     return request.config.getoption('--browser')
+#
+# # It is hook for adding Environment info to HTML report
+# def pytest_configure(config): # For adding information hook isn't required
+#     config.stash[metadata_key]['Project Name'] = 'Tutorial Ninja'
+#     config.stash[metadata_key]['Module Name'] = 'CustRegistration'
+#     config.stash[metadata_key]['Tester Name'] = 'Ola'
+#
+# # It is hook for delete/modify Environment info to HTML report
+# @pytest.hookimpl(optionalhook=True)
+# # @pytest.mark.optionalhook # Deprecated
+# def pytest_metadata(metadata): # For modifying hook is required above
+#     metadata.pop("Python", None)  # pop means remove or delete
+#     metadata.pop("Plugins", None)
+#
+# # Specifying report folder location and save report with timestamp
+# @pytest.hookimpl(tryfirst=True) # Using hook for one configuration which is used from the scratch to generate this report dynamically and it's automatically executed
+# def pytest_configure(config):
+#     config.option.htmlpath = (os.path.dirname(os.getcwd()) + "//HybridFramework//reports//"+datetime.now().strftime("%d-%m-%Y %H-%M-%S")+".html")
+#     # Your existing code
--- utilities/readProperties.py
+++ utilities/readProperties.py
@@ -3,18 +3,43 @@ import configparser
 
 config = configparser.RawConfigParser()
 config.read(os.path.dirname(os.getcwd())+"//HybridFramework//configurations//config.ini")
+
 class ReadConfig():
     @staticmethod
+    def getEnvironment():
+        return config.get('commonInfo', 'environment')
+
+    @staticmethod
     def getApplicationURL():
-        url=(config.get('commonInfo','baseURL'))
-        return url
+        return config.get('commonInfo','baseURL')
+
     @staticmethod
     def getUseremail():
-        username=(config.get('commonInfo','email'))
-        return username
+        return config.get('commonInfo','email')
+
     @staticmethod
     def getPassword():
-        password = (config.get('commonInfo', 'password'))
-        return password
+        return config.get('commonInfo', 'password')
+
+    @staticmethod
+    def getEnvironment():
+        return config.get('commonInfo', 'environment')
+
+
+# config = configparser.RawConfigParser()
+# config.read(os.path.dirname(os.getcwd())+"//HybridFramework//configurations//config.ini")
+# class ReadConfig():
+#     @staticmethod
+#     def getApplicationURL():
+#         url=(config.get('commonInfo','baseURL'))
+#         return url
+#     @staticmethod
+#     def getUseremail():
+#         username=(config.get('commonInfo','email'))
+#         return username
+#     @staticmethod
+#     def getPassword():
+#         password = (config.get('commonInfo', 'password'))
+#         return password
 
 
